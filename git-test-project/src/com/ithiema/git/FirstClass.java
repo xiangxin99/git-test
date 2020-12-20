0e13c43d39f6f2d7fe86fc1bd42c8f1c4305dfe1
Min Xiang <884003813@qq.com>
2020-12-20 16:30:28 +0800

    修改firstclass

--- git-test-project/src/com/ithiema/git/FirstClass.java
+++ git-test-project/src/com/ithiema/git/FirstClass.java
@@ -4,10 +4,12 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git world");
+        //hello test
     }
 
     public void test() {
         int i = 1;
         i += 10;
+        System.out.println(i);
     }
 }
